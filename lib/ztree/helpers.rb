a5d49ec49ba13ffbb914c1c095da1250ba57ed51
sunzhimeng <532681765@qq.com>
2014-10-22 10:48:21 +0800

    add initial tree js, make it much more flexible

--- lib/ztree/helpers.rb
+++ lib/ztree/helpers.rb
@@ -1,11 +1,29 @@
 module ActionView 
   module Helpers
-    
 
-    def tree_list(link_title = '', opts = {})
+    def tree_list(link_title = '', opts = {}, ztree_opts = {})
       id, cls_name = opts[:id] || 'tree-list', opts[:class] || 'menus_ztree ztree small' 
       ul = content_tag :ul, '', {id: id, class: cls_name, style: "width:260px; overflow:auto;"}.merge(opts)
-      init_tree_js(link_title) +  content_tag(:div, ul, class: 'ground fl')
+      return init_tree_js(link_title) +  content_tag(:div, ul, class: 'ground fl') if defined? init_tree_js
+      init_tree(link_title, opts, ztree_opts) +  content_tag(:div, ul, class: 'ground fl')
+    end
+    
+    def init_tree(link_title = "", opts = {}, ztree_opts = {})
+      js = "$(document).ready(function(){
+              if (typeof(treeOpts) == 'undefined'){
+                treeOpts = #{{
+                             zNodes: ztree_opts[:zNodes] || [],
+                             current_tree: "#{opts[:id] || "tree-list"}", 
+                             url: "#{ztree_opts[:url] || "/"}"
+                           }.to_json}
+              }
+              if (typeof(ztreeSetting) == 'undefined'){
+                ztreeSetting = #{ztree_settings(ztree_opts[:settings] || {})}
+              }
+              $.fn.zTree.init($('#' + treeOpts.current_tree), ztreeSetting, treeOpts.zNodes);
+          "
+      js << '});'
+      javascript_tag(js) 
     end
 
     def ztree_settings(opts = {})
@@ -48,7 +66,11 @@ module ActionView
           onClick: "onClick",
         }
       }
-      default_settings.merge(opts).to_json.html_safe.delete("\"").gsub("=>",": ")
+      h_to_j default_settings.merge(opts)
+    end
+    
+    def h_to_j(h)
+      h.to_json.html_safe.delete("\"").gsub("=>",": ")
     end
   end
 end
---
+++ lib/ztree/version.rb
@@ -0,0 +1,5 @@
+module Ztree
+  def self.version
+    "0.0.8"
+  end
+end
